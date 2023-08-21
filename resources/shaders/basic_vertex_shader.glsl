dda1b34512b48fab3d03724f91380342f0c4bfae
Kamil Kopryk <koprykkamil@gmail.com>
2023-08-21 14:13:55 +0200

    Add shaders
    Signed-off-by: Kamil Kopryk <koprykkamil@gmail.com>

---
+++ resources/shaders/basic_vertex_shader.glsl
@@ -0,0 +1,21 @@
+
+#version 330 core
+
+layout(location=0) in vec4 position;
+layout(location=1) in vec3 color;
+
+uniform mat4 u_model;
+uniform mat4 u_view;
+uniform mat4 u_projection;
+uniform mat4 u_mvp;
+
+out vec3 vertexColor;
+
+void main()
+{
+    
+    //gl_Position = u_projection * u_view * u_model * u_position;
+    gl_Position = u_mvp * position;
+    vertexColor = color;
+
+}
